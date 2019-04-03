e3fc39b30467851679758e4290c51b6ffcefb6f5
AmirReza <dashti.amir2752@gmail.com>
2019-04-03 23:26:07 +0430

    check server ip

--- app/Http/Controllers/API/PushRequestController.php
+++ app/Http/Controllers/API/PushRequestController.php
@@ -90,7 +90,7 @@ return;
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
+            CURLOPT_TIMEOUT => 15,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => "{\n    \"msisdn\": \"989126774496\",\n    \"traceId\": 0,\n    \"contentId\": \"string\",\n    \"serviceName\": \"string\",\n    \"amount\": 0,\n    \"chargeCode\": \"string\",\n    \"description\": \"string\"\n}",
